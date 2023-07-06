f0e6724bb140b58a1ef1d29ed47a15ddd04a4d9f
Yusufdev1998 <yusufdev1998@gmail.com>
2023-07-06 10:54:19 +0500

    db linked

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("kanban", "postgres", "1234", {
-  host: "localhost",
-  dialect: "postgres",
-});
+// const db = new Sequelize("kanban", "postgres", "1234", {
+//   host: "localhost",
+//   dialect: "postgres",
+// });
+
+const db = new Sequelize(
+  "postgres://kanban_5h4b_user:8vZEEih1zweNC1nCLf71NOnAi5Do3hbs@dpg-cij5b0p5rnut2salncog-a.singapore-postgres.render.com/kanban_5h4b"
+);
 
 try {
   await db.authenticate();
