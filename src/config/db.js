191aa93822600a632e578f246a4d4dca6842d872
Yusufdev1998 <yusufdev1998@gmail.com>
2023-07-06 10:56:15 +0500

    link changed

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,7 @@ import { Sequelize } from "sequelize";
 // });
 
 const db = new Sequelize(
-  "postgres://kanban_5h4b_user:8vZEEih1zweNC1nCLf71NOnAi5Do3hbs@dpg-cij5b0p5rnut2salncog-a.singapore-postgres.render.com/kanban_5h4b"
+  "postgres://kanban_5h4b_user:8vZEEih1zweNC1nCLf71NOnAi5Do3hbs@dpg-cij5b0p5rnut2salncog-a/kanban_5h4b"
 );
 
 try {
