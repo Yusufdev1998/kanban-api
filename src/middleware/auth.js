e4b400dddf40a4dfe6a5b6f4d44a04f0f22ee9a8
Yusufdev1998 <yusufdev1998@gmail.com>
2023-07-06 11:22:33 +0500

    at function removed

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,8 +6,9 @@ export function auth(req, res, next) {
   if (!crediantials) {
     return res.status(403).json({ error: "No credentials sent!" });
   } else {
-    console.log(crediantials);
-    const token = crediantials.split(" ")?.at(-1);
+    const split = crediantials.split(" ");
+
+    const token = split[split.length - 1];
     try {
       var decoded = jwt.verify(token, process.env.SECRET);
       req.user = decoded;
